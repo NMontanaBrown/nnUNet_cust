aeb6a1c586d70aea15a57f0c840716a1f231b573
wasserth <j.wasserthal@gmx.de>
2022-08-04 15:16:56 +0200

    update setup.py for pypi (2)

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_namespace_packages
 
 setup(name='nnunet_customized',
       packages=find_namespace_packages(include=["nnunet", "nnunet.*"]),
-      version='1.6.6',
+    #   version='1.6.6',  # original nnunet version
+      version='1.0',  # nnunet_customized version
       description='nnU-Net. Framework for out-of-the box biomedical image segmentation.',
       url='https://github.com/wasserth/nnUNet_cust',
       author='Division of Medical Image Computing, German Cancer Research Center',
