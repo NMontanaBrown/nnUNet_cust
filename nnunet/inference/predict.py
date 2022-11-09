cb8aef6d45de5231d3d034085df60b89d84de1f5
wasserth <j.wasserthal@gmx.de>
2022-11-09 14:47:05 +0100

    add fix to make it work on windows

--- nnunet/inference/predict.py
+++ nnunet/inference/predict.py
@@ -12,7 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-
+import sys
 import argparse
 from copy import deepcopy
 from typing import Tuple, Union, List
@@ -21,7 +21,13 @@ import numpy as np
 from batchgenerators.augmentations.utils import resize_segmentation
 from nnunet.inference.segmentation_export import save_segmentation_nifti_from_softmax, save_segmentation_nifti
 from batchgenerators.utilities.file_and_folder_operations import *
-from multiprocessing import Process, Queue
+if 'win' in sys.platform:
+    #fix for windows platform
+    import pathos
+    Process = pathos.helpers.mp.Process
+    Queue = pathos.helpers.mp.Queue
+else:
+    from multiprocessing import Process, Queue
 import torch
 import SimpleITK as sitk
 import shutil
